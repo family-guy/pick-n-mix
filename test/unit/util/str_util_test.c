3a14f9ca1e11f9422d6cfaefe8b426004b815fc6
family-guy <grking8@gmail.com>
2017-05-08 16:13:37 +0000

    Refactor
    Combine two parser unit tests into single unit test (check length of
    arrays and content of arrays in same test.)

--- test/unit/util/str_util_test.c
+++ test/unit/util/str_util_test.c
@@ -17,27 +17,6 @@
 #include "str_util.h"
 #include "int_array.h"
 
-/** @brief Checks parser returns the correct number of integers.
- *  
- *  "123\n100\n24\n\0" is the example string. As parser allocates memory
- *  manually, once the test is done this memory needs to be freed.
- *
- *  @param tc Pointer to CuTest @c struct.
- *  @return Void.
- *  @see parser
- */
-void parser_test_len(CuTest *tc) {
-	char input[12] = "123\n100\n24\n\0";
-	
-	struct int_array *A_prime = parser(input);
-	
-	int actual = A_prime->len;
-	int expected = 3;
-	
-	CuAssertIntEquals(tc, expected, actual);
-	free(A_prime);
-}
-
 /** @brief Checks parser returns the correct integers.
  *  
  *  "123\n100\n24\n\0" is the example string. As parser allocates memory
@@ -47,18 +26,25 @@ void parser_test_len(CuTest *tc) {
  *  @return Void.
  *  @see parser
  */
-void parser_test_vals(CuTest *tc) {
+void parser_test(CuTest *tc) {
 	char input[12] = "123\n100\n24\n\0";
 	
 	struct int_array *A_prime = parser(input);
 	
 	int *actual = A_prime->A;
+	int actual_len = A_prime->len;
 	int expected[] = {123, 100, 24};
+	int expected_len = 3;
 	
 	int cond = 1;
-	for (int i = 0; i < 3; i++) {
-		if (!actual[i] == expected[i]) {
-			cond = 0;
+	if (actual_len != expected_len) {
+		cond = 0;
+	}
+	else {
+		for (int i = 0; i < actual_len; i++) {
+			if (!actual[i] == expected[i]) {
+				cond = 0;
+			}
 		}
 	}
 	CuAssertTrue(tc, cond);
@@ -71,8 +57,7 @@ void parser_test_vals(CuTest *tc) {
  */
 CuSuite* str_util_get_suite() {
 	CuSuite* suite = CuSuiteNew();
-	SUITE_ADD_TEST(suite, parser_test_len);
-	SUITE_ADD_TEST(suite, parser_test_vals);
+	SUITE_ADD_TEST(suite, parser_test);
 	return suite;
 }
 
