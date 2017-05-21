72918ffd613916a3e6d62875988cc7be77743994
family-guy <grking8@gmail.com>
2017-05-21 08:18:23 +0000

    Add sorting

---
+++ include/sort_algo.h
@@ -0,0 +1,22 @@
+/** @file sort_algo.h
+ *  @brief Function prototypes for sorting algorithms.
+ *
+ *  @author Guy R. King (grking8@geemail.com)
+ *  @date 21 May 2017
+ *  @bug No known bugs.
+ */
+
+#ifndef SORT_ALGO_H
+#define SORT_ALGO_H
+
+#include "int_array.h"
+
+/** @brief Sorts an array of integers into ascending order in-place using bubble
+ *         sort.
+ *
+ *  @param A_prime The array to sort.
+ *  @return Void.
+ */
+void sort_bubble(struct int_array *);
+
+#endif
---
+++ src/algo/sort_algo.c
@@ -0,0 +1,26 @@
+/** @file sort_algo.c
+ *  @brief Function definitions for sorting algorithms.
+ *
+ *  Each function implements an algorithm to sort an array of integers.
+ *
+ *  @author Guy R. King (grking8@geemail.com)
+ *  @date 21 May 2017
+ *  @see include/int_array.h
+ *  @bug No known bugs.
+ */
+
+#include "int_array.h"
+
+/** @brief Sorts an array of integers into ascending order in-place using bubble
+ *         sort.
+ *
+ *  Pairs of elements are compared and swapped until the correct order is
+ *  reached.
+ *
+ *  @param A_prime The array to sort.
+ *  @return Void.
+ *  @see int_array
+ */
+void sort_bubble(struct int_array *A_prime) {
+	return;
+}
