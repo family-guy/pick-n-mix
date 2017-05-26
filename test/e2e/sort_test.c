7ef8e19323d32be28b86cb500caa76d6a2175da0
family-guy <grking8@gmail.com>
2017-05-26 09:26:08 +0000

    e2e test for sorting function
    Insertion sort.

--- test/e2e/sort_test.c
+++ test/e2e/sort_test.c
@@ -48,6 +48,34 @@ void sort_bubble_test(CuTest *tc) {
 	free(A_prime);
 }
 
+/** @brief e2e test of insertion sort.
+ *  
+ *  The test data consists of 100,000 integers in a text file. The data is read,
+ *  parsed into an array, and sorted in-place. Checks there are no errors, the 
+ *  user's patience exceeds the runtime, and the array's order.
+ *
+ *  @param tc Pointer to CuTest @c struct.
+ *  @return Void.
+ *  @see read_file
+ *  @see parser
+ *  @see sort_ins
+ */
+void sort_ins_test(CuTest *tc) {
+	const char *path = "./test/e2e/data/IntegerArray.txt";
+	char *fs = read_file(path);
+	struct int_array *A_prime = parser(fs);
+	sort_ins(A_prime);
+	int cond = 1;
+	for (int i = 0; i < A_prime->len - 1; i++) {
+		if (A_prime->A[i] > A_prime->A[i + 1]) {
+			cond = 0;
+		}
+	}
+	CuAssertTrue(tc, cond);
+	free(fs);
+	free(A_prime);
+}
+
 /** @brief Returns a test suite containing the e2e tests in this file.
  *  
  *  Adds each e2e test via SUITE_ADD_TEST.
@@ -57,5 +85,6 @@ void sort_bubble_test(CuTest *tc) {
 CuSuite* sort_get_suite() {
 	CuSuite* suite = CuSuiteNew();
 	SUITE_ADD_TEST(suite, sort_bubble_test);
+	SUITE_ADD_TEST(suite, sort_ins_test);
 	return suite;
 }
