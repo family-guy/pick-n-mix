e5a809d427e09cf49f5e274088085eb054ed5bc1
family-guy <grking8@gmail.com>
2017-06-03 13:14:22 +0000

    e2e test for sorting function
    Selection sort.

--- test/e2e/sort_test.c
+++ test/e2e/sort_test.c
@@ -84,6 +84,36 @@ void sort_ins_test(CuTest *tc) {
 	free(A_prime);
 }
 
+/** @brief e2e test of selection sort.
+ *  
+ *  The test data consists of 100,000 integers in a text file. The data is read,
+ *  parsed into an array, and sorted in-place. Checks there are no errors, the 
+ *  user's patience exceeds the runtime, and the array's order.
+ *
+ *  @param tc Pointer to CuTest @c struct.
+ *  @return Void.
+ *  @see read_file
+ *  @see parser
+ *  @see sort_select
+ */
+void sort_select_test(CuTest *tc) {
+	clock_t start = clock();
+	const char *path = "./test/e2e/data/IntegerArray.txt";
+	char *fs = read_file(path);
+	struct int_array *A_prime = parser(fs);
+	sort_select(A_prime);
+	int cond = 1;
+	for (int i = 0; i < A_prime->len - 1; i++) {
+		if (A_prime->A[i] > A_prime->A[i + 1]) {
+			cond = 0;
+		}
+	}
+	PRINT_EXEC_TIME(start, __func__);
+	CuAssertTrue(tc, cond);
+	free(fs);
+	free(A_prime);
+}
+
 /** @brief Returns a test suite containing the e2e tests in this file.
  *  
  *  Adds each e2e test via SUITE_ADD_TEST.
@@ -94,5 +124,6 @@ CuSuite* sort_get_suite() {
 	CuSuite* suite = CuSuiteNew();
 	SUITE_ADD_TEST(suite, sort_bubble_test);
 	SUITE_ADD_TEST(suite, sort_ins_test);
+	SUITE_ADD_TEST(suite, sort_select_test);
 	return suite;
 }
